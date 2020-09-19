843abc9e988a87b43acaf9bc936658bd39b4648f
mj <>
2020-09-19 15:03:21 +0530

    add static template

--- myfirstapp/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def display(request):
-    return HttpResponse("<h1>My First Django App</h1>")
